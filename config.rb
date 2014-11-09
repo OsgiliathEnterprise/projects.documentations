2a22ea87ddc11519e4f4479553f40c14a01e15ba
charlie <cmordant1@gmail.com>
2014-11-10 00:19:15 +0100

    initial documtation

---
+++ config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/net.osgiliath.parent/"
+http_images_path = "/net.osgiliath.parent/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/net.osgiliath.parent/fonts"
+css_dir = "public/net.osgiliath.parent/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
